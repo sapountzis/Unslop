e119d749f91dbdccceb1287419df20f146b37eed
sapountzis <sapountzis.andreas@gmail.com>
2026-03-13 15:35:18 +0200

    chore(website): upgrade to Astro 6 and drop unused test deps

--- website/ui-tests/smoke.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-const baseUrl = process.env.UI_CHECK_BASE_URL ?? "http://127.0.0.1:4321";
-
-test("frontend smoke", async ({ page }) => {
-	await page.goto(baseUrl);
-	await expect(page).toHaveTitle(/Unslop|unslop/i);
-});
